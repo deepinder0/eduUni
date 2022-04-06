5e04fef3250ee9bf7fdfa2d9e1baad28e2fa1bd9
Deepinder Singh <deep6421@gmail.com>
2022-04-06 12:28:20 +0530

    1st page

---
+++ script.js
@@ -0,0 +1,8 @@
+// <!-- javascript for transition -->
+var navLinks = document.getElementById("navLinks");
+    function showMenu() {
+      navLinks.style.right = '0';
+    }
+    function hideMenu() {
+      navLinks.style.right = '-200px';
+    }
